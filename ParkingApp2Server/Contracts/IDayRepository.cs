800d39c86c2f7d86f1f2be75ea10fa5d2aae53a4
MatusGramblicka <talvex@gmail.com>
2021-09-15 08:44:20 +0200

    rest of files transfered, deletion bin obj

--- ParkingApp2Server/Contracts/IDayRepository.cs
+++ ParkingApp2Server/Contracts/IDayRepository.cs
@@ -1,5 +1,4 @@
 ﻿using Entities.Models;
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -9,10 +8,6 @@ namespace Contracts
     {
         Task<IEnumerable<Day>> GetAllDaysAsync(bool trackChanges);
         Task<Day> GetDayAsync(string NameDay, bool trackChanges);
-        //void CreateDay(Day day);
         Task<IEnumerable<Day>> GetByNamesAsync(IEnumerable<string> dayNames, bool trackChanges);
-
-        //void AddTenantToDay(Day day, Tenant tenant);
-        //void DeleteDay(Day day);
     }
 }
--- ParkingApp2Server/ParkingApp2Server/Controllers/AccountController.cs
+++ ParkingApp2Server/ParkingApp2Server/Controllers/AccountController.cs
@@ -1,9 +1,16 @@
-﻿using Entities;
+﻿using AutoMapper;
+using Contracts;
+using Entities;
+using Entities.Configuration;
 using Entities.DTO;
+using Entities.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using ParkingApp2Server.Services;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,12 +22,21 @@ namespace ParkingApp2Server.Controllers
 	{
 		private readonly UserManager<User> _userManager;
 		private readonly IAuthenticationService _authenticationService;
+		private readonly IMapper _mapper;
+		private readonly PriviledgedUsersConfiguration _priviledgedUsersSettings;
+		private readonly IRepositoryManager _repository;
 
 		public AccountController(UserManager<User> userManager,
-			IAuthenticationService authenticationService)
+			IAuthenticationService authenticationService,
+			IMapper mapper,
+			IOptions<PriviledgedUsersConfiguration> priviledgedUsersSettings,
+			IRepositoryManager repository)
 		{
 			_userManager = userManager;
 			_authenticationService = authenticationService;
+			_mapper = mapper;
+			_priviledgedUsersSettings = priviledgedUsersSettings.Value;
+			_repository = repository;
 		}
 
 		[HttpPost("register")]
@@ -44,6 +60,11 @@ namespace ParkingApp2Server.Controllers
 			}
 
 			await _userManager.AddToRoleAsync(user, "Viewer");
+
+			// todo create tenant also
+			var tenant = new Tenant { TenantId = user.Email};
+			_repository.Tenant.CreateTenant(tenant);
+			await _repository.SaveAsync();
 
 			return StatusCode(201);
 		}
@@ -77,6 +98,82 @@ namespace ParkingApp2Server.Controllers
 				Token = token,
 				RefreshToken = user.RefreshToken
 			});
+		}
+
+		[HttpGet("Users")]
+		[Authorize(Roles = "Administrator")]
+		public async Task<IActionResult> GetUsers()
+		{
+			var allUsers = _userManager.Users.ToList();			
+
+			var userLite = _mapper.Map<IEnumerable<UserLite>>(allUsers);
+
+			return Ok(userLite);
+		}
+
+		[HttpPost("DeleteUser")]
+		[Authorize(Roles = "Administrator")]
+		public async Task<IActionResult> DeleteUser([FromBody] UserLite userForDeletion)
+		{
+			var user = await _userManager.FindByNameAsync(userForDeletion.Email);
+
+			if (user == null)
+			{
+				return Unauthorized(new AuthResponseDto
+				{
+					ErrorMessage = "Invalid Request"
+				});
+			}
+
+			var logins = await _userManager.GetLoginsAsync(user);
+
+			var rolesForUser = await _userManager.GetRolesAsync(user);
+
+			foreach (var login in logins.ToList())
+			{
+				await _userManager.RemoveLoginAsync(user, login.LoginProvider, login.ProviderKey);
+			}
+
+			if (rolesForUser.Count > 0)
+			{
+				foreach (var item in rolesForUser.ToList())
+				{
+					await _userManager.RemoveFromRoleAsync(user, item);
+				}
+			}
+
+			//Delete User
+			await _userManager.DeleteAsync(user);
+
+			return Ok();
+		}
+
+		[HttpPost("UpdatePriviledgeOfUser")]
+		[Authorize(Roles = "Administrator")]
+		public async Task<IActionResult> UpdatePriviledgeOfUser([FromBody] UserLite userLite)
+		{
+			var user = await _userManager.FindByNameAsync(userLite.Email);
+
+			if (user == null)
+			{
+				return BadRequest();
+			}
+
+			if (userLite.Priviledged)
+			{
+				// How many users are priviledged ones?
+				var allUsers = _userManager.Users.ToList();
+				var priviledgedUsersCount = allUsers.Where(w => w.Priviledged == true)
+						.ToList()
+						.Count;
+				if (priviledgedUsersCount >= _priviledgedUsersSettings.MaxCount)
+					return BadRequest();
+			}			
+
+			user.Priviledged = userLite.Priviledged;
+			await _userManager.UpdateAsync(user);
+
+			return NoContent();
 		}
 	}
 }
