efed6d077a54a43730c0d7497049ede4c8842f81
MatusGramblicka <talvex@gmail.com>
2021-12-04 14:52:32 +0100

    webhooks migrations

--- ParkingApp2Server/ParkingApp2Server/Migrations/20211124102642_webHookSubscription.Designer.cs
+++ ParkingApp2Server/ParkingApp2Server/Migrations/20211124102642_webHookSubscription.Designer.cs
@@ -4,6 +4,7 @@ using Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ParkingApp2Server.Migrations
 {
     [DbContext(typeof(RepositoryContext))]
-    partial class RepositoryContextModelSnapshot : ModelSnapshot
+    [Migration("20211124102642_webHookSubscription")]
+    partial class webHookSubscription
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -1670,15 +1672,15 @@ namespace ParkingApp2Server.Migrations
                     b.HasData(
                         new
                         {
-                            Id = "482f6603-e659-4950-80cb-16d3e7acb26d",
-                            ConcurrencyStamp = "d1e243d9-0eea-447d-91f4-5340c47d90da",
+                            Id = "7846aefe-15e3-48d9-96f0-9554c1b7d355",
+                            ConcurrencyStamp = "610c3ec4-956b-4f9f-9215-d973b4286974",
                             Name = "Viewer",
                             NormalizedName = "VIEWER"
                         },
                         new
                         {
-                            Id = "be1e84cd-df83-4498-8347-3571aceea6a7",
-                            ConcurrencyStamp = "568bc35f-d4c7-40af-917a-c80df6dc3c6b",
+                            Id = "2dcfa671-8065-4754-aef3-1ce3645172ca",
+                            ConcurrencyStamp = "96de0e9e-7a3a-437e-8e84-ec9c3af0e2d4",
                             Name = "Administrator",
                             NormalizedName = "ADMINISTRATOR"
                         });
--- ParkingApp2Server/ParkingApp2Server/Migrations/20211204072205_webhooks.Designer.cs
+++ ParkingApp2Server/ParkingApp2Server/Migrations/20211204072205_webhooks.Designer.cs
@@ -4,6 +4,7 @@ using Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ParkingApp2Server.Migrations
 {
     [DbContext(typeof(RepositoryContext))]
-    partial class RepositoryContextModelSnapshot : ModelSnapshot
+    [Migration("20211204072205_webhooks")]
+    partial class webhooks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
